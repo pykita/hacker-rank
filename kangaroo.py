cf64ed220e972654d37644038b69081dba9004f1
Nikita Bolotin <nikbolotin@gmail.com>
2018-09-09 18:33:03 +0300

    small changes

--- kangaroo.py
+++ kangaroo.py
@@ -3,32 +3,39 @@
 import os
 
 def kangaroo(pos1, jump1, pos2, jump2):
+    print(pos1, jump1, pos2, jump2)
+
     if pos1 == pos2 and jump1 == jump2:
         return 'YES'
     # equation system would be:
-    # pos1 * x + jump1 = pos2 * y + jump2
+    #------------------
+    # jump1 * x + pos1 = jump2 * y + pos2
     # x=y
-    # pos1 * x + jump1 = pos2 * x + jump2
+    #------------------
+    # jump1 * x + pos1 = jump2 * x + pos2
     
     pos_x = pos1 - pos2;
-
+    print('pos_x = %s' % pos_x)
     if pos_x == 0:
         return 'NO'
 
-    jump_x = jump1 - jump2;
+    jump_x = (jump1 - jump2);
+    print('jump_x = %s' % jump_x)
 
-    x = jump_x / pos_x
+    x = pos_x / jump_x
+    print('x = %s' % x)
 
-    return 'YES' if x == int(x) else 'NO'
+    return 'YES' if x == int(x) and x > 0 else 'NO'
 
 if __name__ == '__main__':
 
     # uncomment when deploy
     # x1, v1, x2, v2 = map(int, input().split())
 
-    x1, v1, x2, v2 = map(int, '0 2 5 3'.split())
+    x1, v1, x2, v2 = map(int, '0 3 4 2'.split())
 
     result = kangaroo(x1, v1, x2, v2)
-
-    with open(os.environ['OUTPUT_PATH'], 'w') as fptr:
-        fptr.write(result + '\n')
+    print(result)
+    # with open(os.environ['OUTPUT_PATH'], 'w') as fptr:
+    #     fptr.write(result + '\n')
+    

ae9542680d520f1d8328759900e599eb2c97be1b
Nikita Bolotin <nb@vi.ai.com>
2018-09-11 18:50:22 +0300

    testcases 4 and 7 failed. trying to figure out the reason

--- between_two_sets.py
+++ between_two_sets.py
@@ -9,34 +9,32 @@ def get_all_factors_for_all(n_list):
     def is_factor_for_list(factor):
         return all([n % factor == 0 for n in n_list])
 
-    biggest_number = n_list[-1]
-    set_for_biggest = set(reduce(list.__add__, [[i, biggest_number//i] for i in range(2, int(biggest_number**0.5) + 1) if biggest_number % i == 0]))
+    any_number = n_list[-1]
+    set_for_any = set(reduce(list.__add__, [[i, any_number//i] for i in range(2, int(any_number**0.5) + 1) if any_number % i == 0]))
 
-    return filter(is_factor_for_list, set_for_biggest)
+    return list(filter(is_factor_for_list, set_for_any))
 
 def get_total_x(a, b):
-    res = set(reduce(list.__add__, [list(get_all_factors_for_all(b)) for i in b]))
-    return res
-    #
-    # Write your code here.
-    #
+    factors_for_b = get_all_factors_for_all(b)
 
-if __name__ == '__main__':
-    a = [2, 4]
-    b = [16,32,96]
-    c = get_total_x(a, b)
+    # res = set(reduce(list.__add__, [list(get_all_factors_for_all(b)) for i in b]))
     
     def is_divisor_for_list(divisor):
         return all([divisor % n == 0 for n in a])
+    
+    return len(list(filter(is_divisor_for_list, factors_for_b)))
 
-    print(list(filter(is_divisor_for_list, c)))
-    # n, m = map(int, input().split())
+if __name__ == '__main__':
+    # a = [2,4]
+    # b = [16,72,32]
+    # print(get_total_x(a,b))
+    n, m = map(int, input().split())
 
-    # a = list(map(int, input().rstrip().split()))
+    a = list(map(int, input().rstrip().split()))
 
-    # b = list(map(int, input().rstrip().split()))
+    b = list(map(int, input().rstrip().split()))
 
-    # total = getTotalX(a, b)
+    total = get_total_x(a, b)
 
-    # with open(os.environ['OUTPUT_PATH'], 'w') as f:
-    #     f.write(str(total) + '\n')
+    with open(os.environ['OUTPUT_PATH'], 'w') as f:
+        f.write(str(total) + '\n')

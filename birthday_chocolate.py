4e7cee81d317ea2acc5253bcf3888d9f811d3a8b
Nikita Bolotin <nikbolotin@gmail.com>
2018-09-13 23:01:33 +0300

    done. impressed by error absence

--- birthday_chocolate.py
+++ birthday_chocolate.py
@@ -1,29 +1,30 @@
 #!/bin/python3
 
-import math
 import os
-import random
-import re
-import sys
+from functools import reduce
 
 # Complete the birthday function below.
 def birthday(chocolate_list, day, month, chocolate_length):
-    if month > chocolate_length:
+    if month > chocolate_length or month * 5 < day or month > day:
         return 0
     
     acc = 0
 
-    for i in range(month - 1, chocolate_length - month + 1):
-        
+    for i in range(month - 1, chocolate_length):
+        is_fit = reduce(lambda acc, curr: acc + curr, [chocolate_list[i-j] for j in range(0, month)]) == day
+        acc += 1 if is_fit else 0
+
+    return acc
 
 if __name__ == '__main__':
-    n = int(input().strip())
+    chocolate_length = int(input().strip())
 
-    s = list(map(int, input().rstrip().split()))
+    chocolate_list = list(map(int, input().rstrip().split()))
 
-    d, m = map(int, input().rstrip().split())
+    day, month = map(int, input().rstrip().split())
 
-    result = birthday([1,2,3,4], 10, 4, 4)
+    # result = birthday([1,2,3,4,1,2], 10, 4, 6)
+    result = birthday(chocolate_list, day, month, chocolate_length)
 
-    # with open(os.environ['OUTPUT_PATH'], 'w') as fptr:
-    #     fptr.write(str(result) + '\n')
+    with open(os.environ['OUTPUT_PATH'], 'w') as fptr:
+        fptr.write(str(result) + '\n')

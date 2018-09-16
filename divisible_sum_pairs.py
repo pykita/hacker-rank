0a5bc008063f35e044fc692a308e8778c26b5992
Nikita Bolotin <nikbolotin@gmail.com>
2018-09-16 15:57:59 +0300

    done

--- divisible_sum_pairs.py
+++ divisible_sum_pairs.py
@@ -1,26 +1,25 @@
 #!/bin/python3
 
-import math
 import os
-import random
-import re
-import sys
 
 # Complete the divisibleSumPairs function below.
 def divisibleSumPairs(n, k, ar):
     acc = 0
-    for i in range(0, n - 1):
-        for j in range(i, n - 1):
-            if (i + j) % k == 0:
+    for i in range(0, n):
+        for j in range(i+1, n):
+            if (ar[i] + ar[j]) % k == 0:
                 acc += 1
 
     return acc
 
 if __name__ == '__main__':
-    n, k = map(int, input().split())
+    # n, k = map(int, input().split())
 
-    ar = list(map(int, input().rstrip().split()))
+    # ar = list(map(int, input().rstrip().split()))
+
+    n, k, ar = 6, 3, [1, 3, 2, 6, 1, 2]
 
     result = divisibleSumPairs(n, k, ar)
-    with open(os.environ['OUTPUT_PATH'], 'w') as fptr
-        fptr.write(str(result) + '\n')
+    print(result)
+    # with open(os.environ['OUTPUT_PATH'], 'w') as fptr
+    #     fptr.write(str(result) + '\n')

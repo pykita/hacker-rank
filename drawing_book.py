d06751c7d9c7c53c02d6c9334ba4fd1e11fe0e25
Nikita Bolotin <nikbolotin@gmail.com>
2018-09-23 17:40:53 +0300

    drawing still not done:(

--- drawing_book.py
+++ drawing_book.py
@@ -3,16 +3,15 @@
 import os
 import sys
 
-#
-# Complete the pageCount function below.
-#
+import math
+
 def pageCount(n, p):
     steps = 0
-    if 2 * p < n:
+    if 2 * p <= n:
         steps = p // 2
     else:
-        steps = (n - p) // 2
-        steps += 1 if n % 2 == 0 else 0
+        steps = math.ceil((n - p) / 2)
+        # steps += 1 if n % 2 == 0 else 0
     return  steps
 
 if __name__ == '__main__':
@@ -20,7 +19,7 @@ if __name__ == '__main__':
 
     # p = int(input())
 
-    n, p = 6, 5
+    n, p = 6, 6
 
     result = pageCount(n, p)
     print(result)
